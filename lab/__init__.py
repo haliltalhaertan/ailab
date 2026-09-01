17dfe79fdc78f672ea529ca227c247cf666346e5
Halil Talha Ertan <haliltalhaertan@mail.com>
2026-09-01 14:36:29 +0300

    Export theorem research components

--- lab/__init__.py
+++ lab/__init__.py
@@ -1,6 +1,26 @@
 from lab.agent import Agent
 from lab.client import LLMClient
+from lab.literature import LiteratureClient, Paper
 from lab.orchestrator import Orchestrator
+from lab.research_state import ResearchItem, ResearchState
+from lab.theorem_lab import TheoremResearchLab
+from lab.tools import LeanTool, ResearchToolbox, ScriptTool, ToolResult, TropicalGridTool, Z3Tool
 from lab.trace import Trace
 
-__all__ = ["Agent", "LLMClient", "Orchestrator", "Trace"]
+__all__ = [
+    "Agent",
+    "LLMClient",
+    "LiteratureClient",
+    "Paper",
+    "Orchestrator",
+    "ResearchItem",
+    "ResearchState",
+    "TheoremResearchLab",
+    "ResearchToolbox",
+    "ScriptTool",
+    "Z3Tool",
+    "LeanTool",
+    "ToolResult",
+    "TropicalGridTool",
+    "Trace",
+]
