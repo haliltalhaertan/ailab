1bf306ae37a3fdbf4715eae7605a13e152be5b50
Halil Talha Ertan <haliltalhaertan@mail.com>
2026-09-01 22:34:09 +0300

    Export integrity-hardened theorem engine

--- lab/__init__.py
+++ lab/__init__.py
@@ -2,11 +2,11 @@ from lab.agent import Agent
 from lab.client import LLMClient
 from lab.code_experiment import CodeExperimentRunner, GuardedExperimentWorkspace
 from lab.integrity import ProjectBusyError, ProjectRunLock
+from lab.integrity_theorem_lab import TheoremResearchLab
 from lab.literature import LiteratureClient, LiteratureSearchEmpty, Paper
 from lab.orchestrator import Orchestrator
 from lab.research_state import ResearchItem, ResearchState
 from lab.run_controller import ResearchPaused, ResearchStopped
-from lab.theorem_engine import TheoremResearchLab
 from lab.tool_registry import ToolRegistry
 from lab.tools import LeanTool, ResearchToolbox, ScriptTool, ToolResult, TropicalGridTool, Z3Tool
 from lab.trace import Trace
